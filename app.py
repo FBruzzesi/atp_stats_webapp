9abdfb7a576adeec248e84ec4ff12403a30f0d69
FBruzzesi <fbruzzesi@libero.it>
2020-12-16 13:01:10 +0100

    md change

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ markdown = """
 **Data Attribution:** The data used here is (part of) the amazing dataset created by [**Jeff Sackmann**](http://www.jeffsackmann.com/) 
 (Check out his [github repository](https://github.com/JeffSackmann/tennis_atp))
 
-**Data Usage: ** In particular, I am using atp singles from 1995 to 2020 (pre-covid) data. I am currently working towards a solution for independent data gathering.
+**Data Usage: ** In particular, I am using atp singles from 1995 to 2020 data. I am currently working towards an independent data gathering solution.
 
 **Bug Fix:** This is a MVP which I had fun developing, mostly on weekends, for personal use.  Therefore I am sure it is possible to find bugs and non-working interactions. 
 If you find any or just want to get in touch with me, please feel free to reach out by [Linkedin](https://www.linkedin.com/in/francesco-bruzzesi/)
